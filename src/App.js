b24daaad54661eab676a5f7d786165764ba4e775
nz-nz <>
2020-04-26 00:28:37 +0300

    presentational_component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+    Container,
+    Row,
+    Col,
+    Card,
+    CardImg,
+    CardText,
+    CardBody,
+    CardTitle,
+    CardSubtitle,
+    Button,
+    CardLink,
+} from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            list: [], // список комиксов
+            watched: [], // список смотрел / не смотрел
+            errState: null, // состояние запроса - есть ошибка / всё ок
+        }
+    }
+
+    componentDidMount() {
+
+        const movies = fetch('http://api.tvmaze.com/search/shows?q=batman');
+
+        movies
+            .then((data) => {
+                return data.json()
+            })
+            .then((data) => {
+
+                console.log(data);
+                this.setState({
+                    list: data || [],
+                })
+            })
+            .catch((e) => {
+                console.log("REQUEST ERROR: ", e)
+            });
+
+    }
+
+    render() {
+
+        return (
+            <>
+                <Container>
+                    <Row>
+                        <Col><h1>Batman Movies</h1></Col>
+                    </Row>
+                    {/*<Row>*/}
+                    {/*    <Col>*/}
+                    {/*        {*/}
+                    {/*            JSON.stringify(this.state.list)*/}
+                    {/*        }*/}
+                    {/*    </Col>*/}
+                    {/*</Row>*/}
+
+                <div className="root_card">
+                    {
+                        this.state.list.map(({
+                            show,
+                                             }) =>{
+                                return (
+                                        <Card key={ show.id } className="card_div">
+                                            <CardImg top width="100%" src={ show.image.medium } alt="Card image cap" />
+                                            <CardBody>
+                                                <CardTitle><h3>{ show.name }</h3></CardTitle>
+                                                <CardText>{  show.summary  }</CardText>
+                                                <CardSubtitle>{ show.premiered }</CardSubtitle>
+                                                <CardLink href={ show.url }>Card Link</CardLink>
+                                            </CardBody>
+                                            <CardBody>
+                                                <Container>
+                                                    <Row>
+                                                        <Col><Button>Button1</Button></Col>
+                                                        <Col><Button>Детали</Button></Col>
+                                                    </Row>
+                                                </Container>
+                                            </CardBody>
+                                        </Card>
+                                );
+                            }
+                        )
+                    }
+                </div>
+                </Container>
+            </>
+        )
+    }
 }
 
 export default App;
---
+++ src/components/BatmanCard.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+    Button,
+    Card,
+    CardBody,
+    CardImg,
+    CardLink,
+    CardSubtitle,
+    CardText,
+    CardTitle,
+    Col,
+    Container,
+    Row
+} from "reactstrap";
+
+export const MyComponent = props => {
+
+    const {
+        image,
+        name,
+        summary,
+        premiered,
+        url,
+    } = props;
+
+    return (
+        <Card className="card_div">
+            <CardImg top width="100%" src={ image } alt="Card image cap" />
+            <CardBody>
+                <CardTitle><h3>{ name }</h3></CardTitle>
+                <CardText>{  summary  }</CardText>
+                <CardSubtitle>{ premiered }</CardSubtitle>
+                <CardLink href={ url }>Card Link</CardLink>
+            </CardBody>
+            <CardBody>
+                <Container>
+                    <Row>
+                        <Col><Button>Button1</Button></Col>
+                        <Col><Button>Детали</Button></Col>
+                    </Row>
+                </Container>
+            </CardBody>
+        </Card>
+    );
+};
+
+MyComponent.propTypes = {
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    premiered: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+};
+
+MyComponent.defaultProps ={
+    image: '',
+    name: '',
+    summary: '',
+    premiered: '',
+    url: '',
+}
