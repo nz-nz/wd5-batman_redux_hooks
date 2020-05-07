a8f43f39f58b108b14f8e5af70d448233fc01559
nz-nz <>
2020-05-07 03:07:21 +0300

    add details page

--- src/pages/details.jsx
+++ src/pages/details.jsx
@@ -1,12 +1,81 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useSelector } from "react-redux";
+import {
+    Table,
+    Row,
+    Col,
+} from 'reactstrap';
+
+const TableRow = (item) => {
+
+    const dataValidHelper = (localItem) => { // функция-помошник для проверки данных, чтобы не дублировать код
+
+        if(!localItem) {
+            return ' - ';
+        }
+
+        return localItem;
+    };
+
+    return Object.entries(item).map((localItem) => {
+        const data = localItem[1];
+        let textData = dataValidHelper(data);
+
+        if (Array.isArray(data)) {
+            textData = data.join(", ");
+
+        } else if (typeof data === "object") {
+            console.log("DATA = ", data);
+            textData = !!data && Object.values(data).map((subItem) => {
+
+                if (Array.isArray(subItem)) {
+                    return subItem.join(", ");
+                } else return JSON.stringify(subItem);
+            });
+        }
+
+
+        return (
+            <tr>
+                <td><strong>{ localItem[0] }</strong></td>
+                <td>{ textData }</td>
+            </tr>);
+    });
+};
 
 function DetailsPage(props) {
+
+    const moviesListRedux = useSelector((store) => (store.app.movies || []));
+    const selectedItem = moviesListRedux.filter((item) => (item.show.id == props.match.params.code));
+    console.log(selectedItem);
+
     return (
-        <div>
-            <h1>Show Details</h1>
-            <h2>{ props.match.params.code }</h2>
-        </div>
+        <>
+            <Row>
+                <Col>
+                    <h1>Show Details</h1>
+                    <h3>{ props.match.params.code }</h3>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Table>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>info</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            TableRow((selectedItem && selectedItem[0] && selectedItem[0].show) || {})
+                        }
+                        </tbody>
+                    </Table>
+                </Col>
+            </Row>
+        </>
     );
 }
 
